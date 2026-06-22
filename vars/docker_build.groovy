eb453a30e19ccca863fc716529659787e8d2630e
Varun <varunhirapara5291@gmail.com>
2026-06-22 15:50:11 +0530

    Refactor docker_build function parameter names

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,3 @@
-// Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String projectName,String imageTag,String dockerHubUser){
+    sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
 }
